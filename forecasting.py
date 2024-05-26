f7f2c3b6023b40189397109ed5b00d4d1972d7a4
polcord <paul_cordova94@hotmail.com>
2024-05-26 18:26:05 -0500

    one plot

--- forecasting.py
+++ forecasting.py
@@ -4,7 +4,7 @@ from sktime.performance_metrics.forecasting import mean_absolute_percentage_erro
 from get_stock_data import get_info
 from sktime.forecasting.base import ForecastingHorizon
 import pandas as pd
-
+from sktime.utils.plotting import plot_series
 
 fecha_inicio = '2021-01-01'
 
@@ -12,15 +12,10 @@ df = get_info('GOLD', fecha_inicio)
 df.index = df.index.to_period('B')
 df_serie = df.loc[:, 'Close']
 
-print(df_serie)
-
 # Cargar los datos
 
 # Dividir los datos en conjuntos de entrenamiento y prueba
 y_train, y_test = temporal_train_test_split(df_serie, test_size=0.10)
-print(y_test)
-
-
 
 
 # Crear y ajustar el modelo de pronóstico
@@ -28,18 +23,16 @@ forecaster = NaiveForecaster(strategy="last")
 forecaster.fit(y_train)
 
 
-# Crear un horizonte de pronóstico de 10 días
+# Crear un horizonte de pronóstico 
 fh = ForecastingHorizon(
     pd.PeriodIndex(pd.bdate_range(start=str(y_test.index[0]), periods=len(y_test), freq="B")), is_relative=False
 )
-fh
 
 
 # Realizar el pronóstico
 y_pred = forecaster.predict(fh=fh)
-print(y_pred)
+y_pred = y_pred.rename('Prediction')
 
-print(len(y_test), len(y_pred))
 
 # Calcular el error de pronóstico
 error = smape_loss(y_test, y_pred)
@@ -49,28 +42,23 @@ print(f'Error de pronóstico: {error}')
 # different_indices = y_test.index.difference(y_pred.index)
 # print(different_indices)
 
+# plotting for illustration
+plot_series(y_train, y_test, y_pred, labels=["y_train", "y_test", "y_pred"])
 
+# import matplotlib.pyplot as plt
 
-import matplotlib.pyplot as plt
+# # Crear un DataFrame para las predicciones
+# df_total = df.join(y_pred, how='left')
+# df_total.index = df_total.index.to_series().astype(str)
 
-# Crear un DataFrame para las predicciones
-y_pred = y_pred.rename('Prediction')
-# print(y_pred)
-# Concatenar las series original y de predicciones
-# df_total = pd.concat([df, y_pred], axis=1)
 # print(df_total)
 
-df_total = df.join(y_pred, how='left')
-df_total.index = df_total.index.to_series().astype(str)
-
-print(df_total)
-
-# Graficar las series
-plt.figure(figsize=(10,6))
-plt.plot(df_total.index, df_total['Close'], label='Original')
-plt.plot(df_total.index, df_total['Prediction'], label='Predicciones')
-plt.title('Precio de Accion: Original vs Predicciones')
-plt.xlabel('Fecha')
-plt.ylabel('Precio')
-plt.legend()
-plt.show()
+# # Graficar las series
+# plt.figure(figsize=(10,6))
+# plt.plot(df_total.index, df_total['Close'], label='Original')
+# plt.plot(df_total.index, df_total['Prediction'], label='Predicciones')
+# plt.title('Precio de Accion: Original vs Predicciones')
+# plt.xlabel('Fecha')
+# plt.ylabel('Precio')
+# plt.legend()
+# plt.show()
